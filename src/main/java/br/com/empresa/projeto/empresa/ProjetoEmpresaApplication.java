db089ea307fc568c2c33d1236432229ad43def69
Cup-Grape <feliposs11@gmail.com>
2021-09-29 21:06:54 -0300

    Primeiro commit

---
+++ src/main/java/br/com/empresa/projeto/empresa/ProjetoEmpresaApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.projeto.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEmpresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empresa/projeto/empresa/ProjetoEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.projeto.empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
